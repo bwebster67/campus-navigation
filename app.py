22438bf43178da2659cf9711330d2e5cc6a0cebd
Ben Webster <bkwebster@willamette.edu>
2024-12-07 16:53:44 -0800

    changed the host to 0.0.0.0

--- app.py
+++ app.py
@@ -57,5 +57,5 @@ def home():
     
 
 if __name__ == "__main__":
-    serve(app, host='127.0.0.1', port=8000)
+    serve(app, host='0.0.0.0', port=8000)
     # waitress-serve --host 127.0.0.1 hello:app
